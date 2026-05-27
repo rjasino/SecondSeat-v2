5deec9e91709b8ea45c2458c7971d2fe6e6d135c
Rob Asino <45211912+rjasino@users.noreply.github.com>
2026-05-27 11:39:03 +0800

    fix: mongoose connection error and package version conflicts (#7)
    * fix: mongoose connection error and package import
    
    * chore: adjust package.json of conflicting version within the monorepo

---
+++ packages/db/src/connect.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __mongooseCache:
+    | { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null }
+    | undefined;
+}
+
+if (!global.__mongooseCache) {
+  global.__mongooseCache = { conn: null, promise: null };
+}
+
+export async function connectDB(): Promise<typeof mongoose> {
+  const cache = global.__mongooseCache!;
+
+  if (cache.conn) return cache.conn;
+
+  if (!cache.promise) {
+    cache.promise = mongoose.connect(process.env["MONGO_URL"]!);
+  }
+
+  cache.conn = await cache.promise;
+  return cache.conn;
+}
